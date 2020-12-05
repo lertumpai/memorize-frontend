6f42a734c58e64dd31e9f10164a9ddf6a89fe1d2
ninew <new-sorawit@outlook.com>
2020-12-05 16:07:17 +0700

    reset git

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const nextConfig = {
+  publicRuntimeConfig: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
+}
+
+module.exports = nextConfig
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import getConfig from 'next/config'
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>
+      index
+    </div>
+  )
+}
+
+export default IndexPage
