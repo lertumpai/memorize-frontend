e0dc42976e4e9f00b46d19579e85b5a289236de5
ninew <new-sorawit@outlook.com>
2020-12-05 12:54:14 +0700

    old setup

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  development: {
+    base_url: 'http://localhost:5000'
+  },
+  production: {
+    base_url: 'https://new-farm-social.herokuapp.com'
+  }
+}
+const env = process.env.NODE_ENV || 'development'
+
+export default config[env]
