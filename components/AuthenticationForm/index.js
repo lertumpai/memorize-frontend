027c11d975150a1166d36d5a4fab9996a8a05cac
ninew <new-sorawit@outlook.com>
2020-12-06 13:12:59 +0700

    register form

--- components/AuthenticationForm/index.js
+++ components/AuthenticationForm/index.js
@@ -21,24 +21,6 @@ const RegisterForm = () => {
     setForm({ username: '', password: '' })
   }
 
-  function UsernameForm() {
-    return (
-      <Form.Group controlId='username'>
-        <Form.Label>Username</Form.Label>
-        <Form.Control type='text' placeholder='Username' value={form.username} onChange={onChange} />
-      </Form.Group>
-    )
-  }
-
-  function PasswordForm() {
-    return (
-      <Form.Group controlId='password' className='pt-4'>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' value={form.password} onChange={onChange} />
-      </Form.Group>
-    )
-  }
-
   function ButtonRegisterForm() {
     return (
       <div className='d-flex justify-content-end pt-4'>
@@ -63,8 +45,14 @@ const RegisterForm = () => {
 
   return (
     <Form className='border p-2'>
-      <UsernameForm />
-      <PasswordForm />
+      <Form.Group controlId='username'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control type='text' placeholder='Username' value={form.username} onChange={onChange} />
+      </Form.Group>
+      <Form.Group controlId='password' className='pt-4'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type='password' placeholder='Password' value={form.password} onChange={onChange} />
+      </Form.Group>
       <ButtonForm />
     </Form>
   )
