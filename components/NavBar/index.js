c91a99222716796251ad64e6ff263639cb2842c4
ninew <new-sorawit@outlook.com>
2020-12-07 11:36:10 +0700

    navbar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -2,14 +2,16 @@ import { Navbar, Nav } from 'react-bootstrap'
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+    <Navbar className='d-flex justify-content-end' collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Navbar.Toggle className='mr-3' aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mx-lg-auto'>
-          <Nav.Link className='px-4' href='/articles'>Home</Nav.Link>
-          <Nav.Link className='px-4' href='/profile'>Profile</Nav.Link>
-          <Nav.Link className='px-4' href='/'>Logout</Nav.Link>
-        </Nav>
+        <div className='d-flex justify-content-center m-lg-auto'>
+          <Nav>
+            <Nav.Link className='px-4' href='/articles'>Home</Nav.Link>
+            <Nav.Link className='px-4' href='/profile'>Profile</Nav.Link>
+            <Nav.Link className='px-4' href='/'>Logout</Nav.Link>
+          </Nav>
+        </div>
       </Navbar.Collapse>
     </Navbar>
   )
