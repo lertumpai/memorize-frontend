2c0b6bae18d7b44cab6c5e37d1894dc332e7638b
ninew <new-sorawit@outlook.com>
2020-12-07 11:12:05 +0700

    WIP

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -6,9 +6,9 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='mx-lg-auto'>
-          <Nav.Link href='/articles'>Home</Nav.Link>
-          <Nav.Link href='/articles'>Articles</Nav.Link>
-          <Nav.Link href='/profile'>Profile</Nav.Link>
+          <Nav.Link className='px-4' href='/articles'>Home</Nav.Link>
+          <Nav.Link className='px-4' href='/profile'>Profile</Nav.Link>
+          <Nav.Link className='px-4' href='/'>Logout</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
