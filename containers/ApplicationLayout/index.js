1b0ff16da5be80914946e53e89224186e1c7c252
sorawit <slert@sertiscorp.com>
2020-12-09 16:46:56 +0700

    restyle application layout

--- containers/ApplicationLayout/index.js
+++ containers/ApplicationLayout/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import NavBar from '../../components/NavBar'
 
+import './style.scss'
+
 const Index = ({ children }) => {
   return (
     <>
       <NavBar />
-      {children}
+      <div className='application-layout-memorize'>
+        {children}
+      </div>
     </>
   )
 }
