d02624f0733006e7ff2cdb1f71cbf1455b03a5dd
ninew <new-sorawit@outlook.com>
2020-12-10 16:37:41 +0700

    navebar style

--- components/ProfileForm/index.js
+++ components/ProfileForm/index.js
@@ -81,7 +81,7 @@ const ProfileForm = () => {
   }
 
   function Saving() {
-    return status === STATUS_LOADING ? <Loading height={200} /> : ''
+    return status === STATUS_LOADING ? <Loading height={400} /> : ''
   }
 
   return (
