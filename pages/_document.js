64bb7d22ccace2b22c6afc70365c7017fc57c836
ninew <new-sorawit@outlook.com>
2020-12-10 17:05:00 +0700

    add fon style

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <Head>
           <title>Memorize</title>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' rel='stylesheet' />
         </Head>
         <body>
         <Main />
