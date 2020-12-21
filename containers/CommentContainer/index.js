b17316755f82e86c526be889394955e0c519e23a
sorawit <slert@sertiscorp.com>
2020-12-21 11:09:01 +0700

    refactor pure component comment

--- containers/CommentContainer/index.js
+++ containers/CommentContainer/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, useCallback, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { STATUS_IDLE, STATUS_SUCCESS } from '../../store/status'
@@ -12,98 +12,129 @@ import Loading from '../../components/Loading/dynamic'
 
 import './style.scss'
 
-const Index = () => {
-  // const dispatch = useDispatch()
-  // const articleId = window.location.pathname.replace('/articles/', '')
-  //
-  // const state = useSelector(state => state)
-  //
-  // const { status } = useSelector(state => state.comments)
-  // const comments = commentSelectors.selectAll(state)
-  //
-  // const [page, setPage] = useState(1)
-  // const loader = useRef(null)
-  // useEffect(() => {
-  //   dispatch(resetStateComments())
-  //   dispatch(queryArticle({ id: articleId }))
-  //
-  //   const options = {
-  //     root: document.querySelector('#application-layout-memorize'),
-  //     rootMargin: '100px',
-  //     threshold: 0.5,
-  //   }
-  //   const observer = new IntersectionObserver(handleObserver, options)
-  //   if (loader.current) {
-  //     observer.observe(loader.current)
-  //   }
-  // }, [])
-  //
-  // const handleObserver = entities => {
-  //   if (entities[0].isIntersecting) {
-  //     setPage(page => page + 1)
-  //   }
-  // }
-  //
-  // useEffect(() => {
-  //   const lastComment = comments ? comments[comments.length - 1] : null
-  //   const pagination = {
-  //     before: lastComment?.createdAt,
-  //     limit: 15,
-  //   }
-  //   dispatch(queryComments({ articleId, pagination }))
-  // }, [page])
-  //
-  // useEffect(() => {
-  //   if (status === STATUS_SUCCESS) {
-  //     dispatch(idleStateComments())
-  //   }
-  // }, [status])
-  //
-  // function CommentContentBoxes() {
-  //   return comments.map(comment => {
-  //     const user = userSelectors.selectById(state, comment.author)
-  //     return <MemorizeContentBox key={comment.id} memorize={comment} user={user} mode={MODE_COMMENT} />
-  //   })
-  // }
-  //
-  // function ContainerLeftCol() {
-  //   const article = articleSelectors.selectById(state, articleId)
-  //   const user = userSelectors.selectById(state, article?.author)
-  //  return (
-  //    <div className='comment-container-col-memorize'>
-  //      <div className='comment-left-col-memorize'>
-  //        <MemorizeCreateContentBox mutateContent={mutateComment} articleId={articleId} />
-  //        <MemorizeContentBox memorize={article} user={user} mode={MODE_ARTICLE} />
-  //      </div>
-  //    </div>
-  //  )
-  // }
-  //
-  // function ContainerRightCol() {
-  //  return (
-  //    <div className='comment-container-col-memorize'>
-  //      <div className='comment-right-col-memorize'>
-  //        {CommentContentBoxes()}
-  //        <div ref={loader}>
-  //          {status !== STATUS_IDLE ? <Loading width={300} /> : ''}
-  //        </div>
-  //      </div>
-  //    </div>
-  //  )
-  // }
-  //
-  // function CommentContainer() {
-  //   return (
-  //     <>
-  //       <div className='comment-container-memorize'>
-  //         {ContainerLeftCol()}
-  //         {ContainerRightCol()}
-  //       </div>
-  //     </>
-  //   )
-  // }
+const Index = ({ articleId }) => {
+  const dispatch = useDispatch()
 
-  return <div>1</div>
+  const [content, setContent] = useState('')
+
+  const state = useSelector(state => state)
+  const { status } = useSelector(state => state.comments)
+  const comments = commentSelectors.selectAll(state)
+  const users = userSelectors.selectAll(state)
+  const articles = articleSelectors.selectAll(state)
+
+  useMemo(() => {
+    if (articleId) {
+      dispatch(queryArticle({ id: articleId }))
+    }
+  }, [articleId])
+
+  const [page, setPage] = useState(1)
+  const loader = useRef(null)
+  useEffect(() => {
+    dispatch(resetStateComments())
+
+    const options = {
+      root: document.querySelector('#application-layout-memorize'),
+      rootMargin: '100px',
+      threshold: 0.5,
+    }
+    const observer = new IntersectionObserver(handleObserver, options)
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
+  }, [])
+
+  const handleObserver = entities => {
+    if (entities[0].isIntersecting) {
+      setPage(page => page + 1)
+    }
+  }
+
+  useEffect(() => {
+    if (status === STATUS_SUCCESS) {
+      dispatch(idleStateComments())
+    }
+  }, [status])
+
+
+  const onContentChange = useCallback(e => {
+    setContent(e.target.value)
+  }, [])
+
+  const onMemorize = useCallback(() => {
+    const memorize = {
+      articleId,
+      content,
+    }
+    dispatch(mutateComment(memorize))
+    setContent('')
+  }, [content])
+
+  const onArticleLike = useCallback(articleId => {
+    console.log(articleId)
+  }, [])
+
+  function ContainerLeftCol() {
+    const article = articleSelectors.selectById(state, articleId)
+    const user = userSelectors.selectById(state, article?.author)
+    return (
+      <div className='comment-container-col-memorize'>
+        <div className='comment-left-col-memorize'>
+          <MemorizeCreateContentBox value={content} onChange={onContentChange} onMemorize={onMemorize} />
+          <MemorizeContentBox key={article?.id} memorize={article} author={user} onLike={onArticleLike}/>
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    if (articleId) {
+      const lastComment = comments ? comments[comments.length - 1] : null
+      const pagination = {
+        before: lastComment?.createdAt,
+        limit: 15,
+      }
+      dispatch(queryComments({ articleId, pagination }))
+    }
+  }, [page, articleId])
+
+  const onCommentLike = useCallback(commentId => {
+    console.log(commentId)
+  }, [])
+
+  const CommentContentBoxes = useCallback(() => {
+    return comments.map(comment => {
+      const user = userSelectors.selectById(state, comment.author)
+      return <MemorizeContentBox key={comment?.id} memorize={comment} author={user} onLike={onCommentLike}/>
+    })
+  }, [comments, users])
+
+  function ContainerRightCol() {
+    return (
+     <div className='comment-container-col-memorize'>
+       <div className='comment-right-col-memorize'>
+         {CommentContentBoxes()}
+         <div ref={loader}>
+           {status !== STATUS_IDLE ? <Loading width={300} /> : ''}
+         </div>
+       </div>
+     </div>
+    )
+  }
+
+  function CommentContainer() {
+    return (
+      <>
+        <div className='comment-container-memorize'>
+          {ContainerLeftCol()}
+          {ContainerRightCol()}
+        </div>
+      </>
+    )
+  }
+
+  return CommentContainer()
 }
 
-export default Index
+export default React.memo(Index)
--- pages/articles/[articleId]/index.js
+++ pages/articles/[articleId]/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import ApplicationLayout from '../../../containers/ApplicationLayout/dynamic'
 import CommentContainer from '../../../containers/CommentContainer/dynamic'
 
 const Index = () => {
+  const router = useRouter()
+  const { articleId } = router.query
+
   return (
     <ApplicationLayout>
-      <CommentContainer />
+      <CommentContainer articleId={articleId} />
     </ApplicationLayout>
   )
 }
