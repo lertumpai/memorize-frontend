cb9cf95223f101a16604091beb464ae99b123fce
ninew <new-sorawit@outlook.com>
2021-01-08 13:25:57 +0700

    test hook docker

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import reduxWrapper from '../store'
 import client from '../utils/graphql-api/client'
 
 import '../public/style/style.scss'
-
+console.log('test v.1')
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={client}>
